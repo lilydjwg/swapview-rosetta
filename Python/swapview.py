18ea0844e7d3a354b98adfa5cad2faf852a45810
lilydjwg <lilydjwg@gmail.com>
2014-12-24 13:34:25 +0800

    Python: 去除对 winterpy 的依赖

--- Python/swapview.py
+++ Python/swapview.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
-# vim:fileencoding=utf-8
 
 import os
 import re
 
-from myutils import filesize
-
 format = "%5s %9s %s"
 totalFmt = "Total: %8s"
+
+def filesize(size):
+  '''将 数字 转化为 xxKiB 的形式'''
+  units = 'KMGT'
+  left = abs(size)
+  unit = -1
+  while left > 1100 and unit < 3:
+    left = left / 1024
+    unit += 1
+  if unit == -1:
+    return '%dB' % size
+  else:
+    if size < 0:
+      left = -left
+    return '%.1f%siB' % (left, units[unit])
 
 def getSwapFor(pid):
   try:
