c0761dd343bcd84ec766b7f7e70db5603b05082b
lilydjwg <lilydjwg@gmail.com>
2014-12-25 21:35:09 +0800

    fix up Lua version

--- Lua/swapview.lua
+++ Lua/swapview.lua
@@ -1,68 +1,83 @@
-require "lfs"
+#!/usr/bin/env lua
+
+require 'lfs'
+
+local format = string.format
+
 function filesize(size)
-	--将数字转换为 xxKiB 的形式
-	local units = { "K", "M", "G", "T" }
-	local value = math.abs(size)
-	local uint = 0
-	while value > 1100 and uint < 4 do
-		value = value / 1024
-		uint = uint + 1
-	end
-	if uint == 0 then
-		return string.format("%dB", size)
-	else
-		if size < 0 then
-			value = -value
-		end
-		return string.format("%f%siB", value, units[uint])
-	end
+  local units = {'K', 'M', 'G', 'T'}
+  local value = math.abs(size)
+  local uint = 0
+  while value > 1100 and uint < 4 do
+    value = value / 1024
+    uint = uint + 1
+  end
+  if uint == 0 then
+    return format('%dB', size)
+  else
+    if size < 0 then
+      value = -value
+    end
+    return format('%.1f%siB', value, units[uint])
+  end
 end
 
 function getSwapFor(pid)
-	local size = 0
-	local swapfile = io.open(string.format("/proc/%s/smaps", pid), "r")
-	if swapfile then
-		for line in swapfile:lines() do
-			if string.sub(line, 1, 5) == "Swap:" then
-				size = size + tonumber(string.match(line, "%d+$") or 0)
-			end
-		end
-	end
-	swapfile:close()
-	local cmd = ""
-	local cmdfile = io.open(string.format("/proc/%s/cmdline", pid), "r")
-	if cmdfile then
-		--cmd = string.gsub(cmdfile:read("*a"), "\x00", " ")
-		cmd = cmdfile:read("*a")
-	end
-	cmdfile:close()
-	return size, cmd
+  local open = io.open
+  local swapfile = open(format('/proc/%s/smaps', pid), 'r')
+  if not swapfile then return 0, '' end
+
+  -- file:lines() fails when it doesn't have the permissions to read the file
+  -- (but does have when opening the file, e.g. /proc/1/smaps)
+  local ok, size = pcall(function()
+    local size = 0
+    for line in swapfile:lines() do
+      if line:sub(1, 5) == 'Swap:' then
+        size = size + tonumber(line:match('%d+'))
+      end
+    end
+    return size
+  end)
+  swapfile:close()
+  if not ok then return 0, '' end
+
+  local cmdfile = open(format('/proc/%s/cmdline', pid), 'r')
+  if not cmdfile then return 0, '' end
+
+  local cmd = cmdfile:read('*a')
+  cmdfile:close()
+  if cmd then
+    -- FIXME: LuaJIT adds spaces to every letters...
+    cmd = cmd:sub(1, #cmd-1):gsub('\x00', ' ')
+  end
+
+  return size * 1024, cmd
 end
 
 function getSwap()
-	local ret = {}
-	for pid in lfs.dir('/proc') do
-		if tonumber(pid) then
-			size, cmd = getSwapFor(pid)
-			if size > 0 then
-				table.insert(ret, { pid, size, cmd })
-			end
-		end
-	end
-	local comp = function(a, b)
-		if a[2] < b[2] then
-			return true
-		end
-	end
-	table.sort(ret, comp)
-	return ret
+  local ret = {}
+  local insert = table.insert
+  for pid in lfs.dir('/proc') do
+    if tonumber(pid) then
+      size, cmd = getSwapFor(pid)
+      if size > 0 then
+        insert(ret, {pid, size, cmd})
+      end
+    end
+  end
+  table.sort(ret, function(a, b)
+    return a[2] < b[2]
+  end)
+  return ret
 end
 
-print("PID", "SWAP", "COMMAND")
+local fmt = "%5s %9s %s"
+local totalFmt = "Total: %8s"
 local results = getSwap()
 local sum = 0
+print(format(fmt, 'PID', 'SWAP', 'COMMAND'))
 for _, v in ipairs(results) do
-	print(v[1], filesize(v[2]), v[3])
-	sum = sum + v[2]
+  print(format(fmt, v[1], filesize(v[2]), v[3]))
+  sum = sum + v[2]
 end
-print("Total: ", filesize(sum))
+print(format(totalFmt, filesize(sum)))
