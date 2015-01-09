5dc1f238d705bf40601b0fcf7bd9397fbbc324da
Jiachen Yang <farseerfc@gmail.com>
2015-01-10 02:13:02 +0900

    fix #38 merged bench.toml and benchmark.toml

--- bench.py
+++ bench.py
@@ -42,10 +42,9 @@ def run_profile(cmd, dir, time_limit, **kwargs):
     out, err = '', ''
     returncode = 0
     start_time = time.time()
-    proc = Popen(['/usr/bin/timeout', str(time_limit)] + cmd,
-                 stdout=PIPE, stderr=PIPE,
-                 cwd=dir, shell=False, universal_newlines=True)
-    out, err = proc.communicate()
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE,
+                cwd=dir, shell=False, universal_newlines=True)
+    out, err = proc.communicate(timeout=time_limit)
     returncode = proc.returncode
     end_usage = resource.getrusage(resource.RUSAGE_CHILDREN)
     end_time = time.time()
@@ -55,19 +54,19 @@ def run_profile(cmd, dir, time_limit, **kwargs):
     return out, err, returncode, usage
 
 
-def bench_profile(profile, ref_result):
+def bench_profile(profile, ref_result_out, verbose, time_field, show_diff_below, **kwargs):
     out, err, ret, usage = run_profile(**profile)
     ratio = SequenceMatcher(None,
                             out.split('\n'),
-                            ref_result.split('\n')).ratio()
+                            ref_result_out.split('\n')).ratio()
     profile['ratio'] = ratio
-    if profile['verbose']:
-        print('\033[1;%dm%24s\033[m(%3d%%): %8.2f %s' %
+    if verbose:
+        print('\033[1;%dm%-24s\033[m(%3d%%): %8.2f %s' %
               (32 if ret == 0 else 31,
                profile['name'], int(ratio*100),
-               usage[profile['time_field']]*1000, err[:-1]))
-        if ret == 0 and ratio < profile['show_diff_below']:
-            print('\n'.join(unified_diff(ref_result.split('\n'), out.split('\n'),
+               usage[time_field]*1000, err[:-1]))
+        if ret == 0 and ratio < show_diff_below:
+            print('\n'.join(unified_diff(ref_result_out.split('\n'), out.split('\n'),
                                          fromfile='reference result',
                                          tofile=profile['name'],
                                          n=0, lineterm='')))
@@ -75,68 +74,71 @@ def bench_profile(profile, ref_result):
 
 
 def load_config():
-    filename = sys.argv[1] if len(sys.argv) > 1 else 'bench.toml'
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'benchmark.toml'
     with open(filename) as configfile:
         config = toml.loads(configfile.read())
     items = config['item']
     default = items['default']
-    reference = config['reference']
+    if 'options' in config:
+        options = config['options']
+    else:
+        options = dict(ref_result = "C",
+                    time_field = "elapsed",
+                    show_diff_below = 0.9,
+                    verbose = True)
     del items['default']
     for name, item in zip(items, items.values()):
         profile = dict(default)
         profile.update(item)
         profile['name'] = name
         items[name] = profile
-        profile['valid_times'] = int(profile['count_limit']*profile['valid_percent']/100)
         for k, v in zip(profile, profile.values()):
             if type(v) is str:
                 profile[k] = Template(v).safe_substitute(**profile)
-    return items, reference
+    return items, options
 
 
 def times2str(item):
     time = item['time']
-    valid = sorted(time)[:item['valid_times']]
+    valid = sorted(time)[:int(item['valid_percent']*len(time)/100)]
     avgv = mean(valid)
-    return ("%s %s [%s]" % (
+    return "%s %s %4d" % (
         '%8.2f' % (avgv*1000),
         ' '.join(('%8.2f' % (x(time)*1000))
             for x in (min, mean, max, stdev)),
-        ' '.join(('%8.2f' % (x*1000))
-            for x in valid)))
+        len(time))
 
 
 def main():
-    items, reference = load_config()
+    items, options = load_config()
     succ, fails = [], []
     for item in sorted(items.values(), key=lambda x:x['name']):
-        ref_result, err, ret, _ = run_profile(**items[reference["result"]])
+        ref_result_out, err, ret, _ = run_profile(**items[options["ref_result"]])
         if ret != 0:
             die("reference implementation failed")
-        out, err, ret, usage = bench_profile(item, ref_result)
-        item['time'] = [usage[item['time_field']]]
+        out, err, ret, usage = bench_profile(item, ref_result_out, **options)
+        item['time'] = [usage[options['time_field']]]
         if ret == 0:
             for i in range(item['count_limit'] - 1):
-                _, err, _, usage = bench_profile(item, ref_result)
-                item['time'].append(usage[item['time_field']])
+                _, err, _, usage = bench_profile(item, ref_result_out, **options)
+                item['time'].append(usage[options['time_field']])
             succ.append(item)
         else:
             item['error'] = err
             fails.append(item)
 
     print(("---------Results--------(Diff):" +
-           "  KMinAvg      Min      Avg      Max    Stdev [K(%d)-samples raw data]") %
-          items[reference["result"]]['valid_times'])
+           "  KMinAvg      Min      Avg      Max    Stdev  Cnt"))
 
-    succ.sort(key=lambda x: sum(sorted(x['time'])[:x['valid_times']]))
+    succ.sort(key=lambda x: sum(sorted(x['time'])[:int(x['valid_percent'] * x['count_limit']/100)]))
     for item in succ:
-        print('\033[1;%dm%24s\033[m(%3d%%): %s' %
-              (33 if item['ratio'] < item['show_diff_below'] else 32,
+        print('\033[1;%dm%-24s\033[m(%3d%%): %s' %
+              (33 if item['ratio'] < options['show_diff_below'] else 32,
                item['name'], int(item['ratio']*100), times2str(item)))
 
     print("------------Fails-------------------")
     for item in fails:
-        print('\033[1;31m%24s\033[m       : %s' %
+        print('\033[1;31m%-24s\033[m       : %s' %
               (item['name'], item['error']))
 
 if __name__=='__main__':
