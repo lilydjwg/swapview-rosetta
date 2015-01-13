9b8fd6e7d0c50182c7447199362c6777b5d2884a
Jiachen Yang <farseerfc@gmail.com>
2015-01-14 04:26:20 +0900

    fixed #3 bash output format

--- Bash/swapview.sh
+++ Bash/swapview.sh
@@ -1,26 +1,59 @@
 #!/bin/bash
 
-cd /proc
-
-for pid in [0-9]*; do
-    command=$(cat /proc/$pid/cmdline 2>/dev/null)
-    [[ $? -ne 0 ]] && continue
-
-    swap=$(
-        awk '
-            BEGIN  { total = 0 }
-            /Swap/ { total += $2 }
-            END    { print total }
-        ' /proc/$pid/smaps 2>/dev/null
-    )
-    [[ $? -ne 0 ]] && continue
-
-    if (( $swap > 0 )); then
-        if [[ "${head}" != "yes" ]]; then
-            echo -e "PID\tSWAP\tCOMMAND"
-            head="yes"
-        fi
-
-        echo -e "${pid}\t${swap}\t${command}"
+function filesize(){
+    size=$1
+    if [[ $1 -le 1100 ]] ; then
+        echo "${size}B"
+        return
     fi
-done
+    arr=($(bc -l <<EOF
+left=$size;
+unit=-1;
+while(left>1100 && unit<3){
+    left /= 1024
+    unit += 1
+}
+left
+unit
+EOF
+))
+    left=${arr[0]}
+    unit=${arr[1]}
+    units="KMGT"
+    printf "%.1f%siB\n" "$left" "${units:$unit:1}"
+}
+
+function getSwap(){
+    sumfile=/tmp/sum$RAMDOM
+    > $sumfile
+    cd /proc
+    (for pid in [0-9]*; do
+        command=$(cat /proc/$pid/cmdline 2>/dev/null | tr '\0' ' ')
+        len=$((${#command}-1))
+        if [[ "${command:$len:1}"x = " "x ]]; then
+            command="${command:0:$len}"
+        fi
+        [[ $? -ne 0 ]] && continue
+
+        swap=$(
+            awk '
+                BEGIN  { total = 0 }
+                /Swap/ { total += $2 }
+                END    { print total }
+            ' /proc/$pid/smaps 2>/dev/null
+        )
+        [[ $? -ne 0 ]] && continue
+
+        if (( $swap > 0 )); then
+            fs=`filesize $((swap*1024))`
+            echo $swap >>$sumfile
+            printf "%5s %9s %s\n" "$pid" "$fs" "$command"
+        fi
+    done) | sort -k2 -h
+    total=$(filesize $(( $(paste -sd+ <$sumfile | bc) *1024)))
+    printf "Total: %8s\n" $total
+    rm $sumfile
+}
+
+printf "%5s %9s %s\n" PID SWAP COMMAND
+getSwap
