21d82c3574226ed7b2a0b21d6003593c02a22400
JaHIY <jaklsy@gmail.com>
2015-01-31 19:51:45 +0800

    rewrite perl script

--- Perl/swapview.pl
+++ Perl/swapview.pl
@@ -2,65 +2,79 @@
 
 use strict;
 use warnings;
+use utf8;
+use autodie;
+use 5.010;
 
-sub filesize{
-    my ($size) = @_;
-    my $units = "KMGT";
-    my $left = abs($size);
-    my $unit = -1;
-    while($left > 1100 and $unit < 3){
-        $left /= 1024;
-        $unit++;
-    }
-    if($unit == -1){
-        return sprintf('%dB', $size);
-    }else{
-        if($size < 0){
-            $left = - $left; 
-        }
-        return sprintf("%.1f%siB", $left, substr($units, $unit, 1));
-    }
-}
+use English qw{ -no_match_vars }; # Avoids regex performance penalty in perl 5.16 and earlier.
 
-sub getSwapFor{
-    my ($pid) = @_;
-    open FILE,"/proc/$pid/cmdline" or return (pid=>$pid, size=>0, comm=>'');
-    my $comm = join("", <FILE>); 
-    close FILE;
-    substr($comm, length($comm) - 1, 1) eq "\0" and chop($comm);
-    $comm =~ s/\0/ /g;
-    open FILE,"/proc/$pid/smaps" or return (pid=>$pid, size=>0, comm=>'');
-    my $s=0;
-    while(<FILE>){
-        if(/^Swap:/){
-            s/[^0-9]//g;
-            $s += $_;
-        }
-    }
-    close FILE;
-    return (pid => $pid, size => $s * 1024, comm =>$comm);
-}
-
-sub getSwap{
-    my @ret = ();
-    opendir (DIR,'/proc') or die $!;
-    while (readdir(DIR)){
-        if(/^[0-9]+$/){
-            my %s = getSwapFor($_);
-            if($s{size} > 0){
-                push @ret, {%s};
+sub get_swap_info_from {
+    my ( $pid_dirs_ref ) = @_;
+    my @results = ();
+    for my $pid ( @{$pid_dirs_ref} ) {
+        my $smaps_file = join q{/}, $pid, q{smaps};
+        my $swap_size = 0;
+        if ( -f $smaps_file and
+             -r $smaps_file and
+             ( $EUID == 0 or -O $smaps_file )
+        ) {
+            open my $smaps_file_fh, '<', $smaps_file;
+            while (my $line = <$smaps_file_fh>) {
+                if ( $line =~ /Swap/ ) {
+                    $swap_size += ( split( /[[:space:]]+/, $line ) )[1];
+                }
             }
+            close $smaps_file_fh;
         }
+        next if ( $swap_size == 0 );
+        my $cmdline_file =  join q{/}, $pid, q{cmdline};
+        my $cmdline;
+        if ( -f $cmdline_file and
+             -r $cmdline_file and
+             ( $EUID == 0 or -O $cmdline_file )
+        ) {
+            open my $cmdline_file_fh, '<', $cmdline_file;
+            $cmdline = do {
+                local $RS;
+                <$cmdline_file_fh>;
+            };
+            close $cmdline_file_fh;
+            $cmdline =~ s/\0$//;
+            $cmdline =~ s/\0/ /g;
+        }
+        push @results, { pid  => $pid,
+                         swap => $swap_size,
+                         cmd  => $cmdline,
+                       };
     }
-    my @sort_ret = sort { $a->{size} <=> $b->{size} } @ret;
-    return @sort_ret;
+    return @results;
 }
 
-my @result = getSwap();
-printf "%5s %9s %s\n", 'PID', 'SWAP', 'COMMAND';
-my $t = 0;
-for my $i (@result){
-    print sprintf("%5s %9s %s\n", $i->{pid}, filesize($i->{size}), $i->{comm});
-    $t += $i->{size};
+sub convert_file_size_from_kB {
+    my ( $size ) = @_;
+    my @units = qw{K M G T};
+    my $unit = 0;
+    while ( $size > 1100 and $unit < 3 ) {
+        $size /= 1024;
+        $unit += 1;
+    }
+    return sprintf( "%.1f%siB", $size, $units[$unit] );
 }
-print sprintf("Total: %8s\n", filesize($t));
+
+sub main {
+    my $printf_format = qq{%5s %9s %s\n};
+    printf $printf_format, qw{PID SWAP COMMAND};
+    opendir( my $proc_dh, q{/proc} );
+    chdir $proc_dh;
+    my @pid_dirs = grep { -d && /^[[:digit:]]+$/xms } readdir $proc_dh;
+    closedir $proc_dh;
+    my @results = sort { $a->{'swap'} <=> $b->{'swap'} } get_swap_info_from( \@pid_dirs );
+    my $total_swap_size = 0;
+    for my $result ( @results ) {
+        $total_swap_size += $result->{'swap'};
+        printf $printf_format, $result->{'pid'}, convert_file_size_from_kB( $result->{'swap'} ), $result->{'cmd'};
+    }
+    printf qq{Total: %s\n}, convert_file_size_from_kB( $total_swap_size );
+}
+
+main
