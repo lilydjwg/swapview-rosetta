73283f5612a9faf00c6e5027858f58f74e1141b5
Ma Kai <crtmike@gmail.com>
2015-05-16 17:02:56 +0800

    Tcl: line-by-line read for better performance

--- Tcl/swapview.tcl
+++ Tcl/swapview.tcl
@@ -1,7 +1,7 @@
 #/usr/bin/tclsh
 
 proc fileSize {bytes {level 0}} {
-	set units {B K M G}
+	set units {B KiB MiB GiB}
 	if {$bytes >= 1024 && $level < 3} {
 		return [fileSize [expr $bytes/1024] [expr $level+1]]
 	}
@@ -19,9 +19,11 @@ proc getSwapInfo {pid} {
 	# return {pid swap-use cmdline}
 	if {![string is digit $pid]} {error error}
 	set cmdline [readFile "/proc/$pid/cmdline"]
-	foreach l [split [readFile "/proc/$pid/smaps"] "\n"] {
-		if {[scan $l "Swap: %d" swapuse]} break
+	set fd [open "/proc/$pid/smaps"]
+	while {1} {
+		if {[scan [gets $fd] "Swap: %d" swapuse]} break
 	}
+	close $fd
 	return [list $pid [expr $swapuse*1024] $cmdline]
 }
 
@@ -30,7 +32,6 @@ puts [format "%5s %9s %s" PID SWAP COMMAND]
 foreach pid [lmap dir [glob /proc/*] {file tail $dir}] {
 	if {![catch {getSwapInfo $pid} info] && [llength $info] == 3 && [lindex $info 1] != 0} {
 		puts [format "%5d %9s %s" [lindex $info 0] [fileSize [lindex $info 1]] [lindex $info 2]]
-		
 	}
 }
 puts [format "Total: %8s" [fileSize $total]]
