1219fe199343ee7ce5aa8b7851ca95df89f9ca0f
Ma Kai <crtmike@gmail.com>
2015-05-16 22:38:04 +0800

    Tcl: some fixes

--- Tcl/swapview.tcl
+++ Tcl/swapview.tcl
@@ -3,9 +3,9 @@
 proc fileSize {bytes {level 0}} {
 	set units {B KiB MiB GiB}
 	if {$bytes >= 1024 && $level < 3} {
-		return [fileSize [expr $bytes/1024] [expr $level+1]]
+		return [fileSize [expr $bytes/1024.0] [expr $level+1]]
 	}
-	return "$bytes[lindex $units $level]"
+	return [format "%.1lf%s" $bytes [lindex $units $level]]
 }
 
 proc readFile {path} {
@@ -18,20 +18,26 @@ proc readFile {path} {
 proc getSwapInfo {pid} {
 	# return {pid swap-use cmdline}
 	if {![string is digit $pid]} {error error}
-	set cmdline [readFile "/proc/$pid/cmdline"]
+	set cmdline [string range [string map {\0 " "} [readFile "/proc/$pid/cmdline"]] 0 end-1]
 	set fd [open "/proc/$pid/smaps"]
 	while {1} {
-		if {[scan [gets $fd] "Swap: %d" swapuse]} break
+		if {[scan [gets $fd] "Swap: %lld" swapuse]} break
 	}
 	close $fd
 	return [list $pid [expr $swapuse*1024] $cmdline]
 }
 
 set total 0
+set all {}
 puts [format "%5s %9s %s" PID SWAP COMMAND]
 foreach pid [lmap dir [glob /proc/*] {file tail $dir}] {
 	if {![catch {getSwapInfo $pid} info] && [llength $info] == 3 && [lindex $info 1] != 0} {
-		puts [format "%5d %9s %s" [lindex $info 0] [fileSize [lindex $info 1]] [lindex $info 2]]
+		lappend all $info
+		incr total [lindex $info 1]
 	}
+}
+set all [lsort -index 1 $all]
+foreach i $all {
+	puts [format "%5s %9s %s" [lindex $i 0] [fileSize [lindex $i 1]] [lindex $i 2]]
 }
 puts [format "Total: %8s" [fileSize $total]]
