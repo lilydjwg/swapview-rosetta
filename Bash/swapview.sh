0aa131ea8c8a5110d80f2c0add09cd74d05439b0
Mingye Wang <arthur200126@gmail.com>
2016-01-29 06:02:24 -0500

    Revert to the old formatting.

--- Bash/swapview.sh
+++ Bash/swapview.sh
@@ -12,7 +12,7 @@ filesize(){
     fi
     # See also ``base ** pow`` (bash) and ``base ^ pow`` (bc).
     for ((pos=0, powed=1; size / powed > 1100 && pos < nunit; pos++; powed *= 1024)); do :; done
-    printf '%.2g%siB\n' "$(bc <<< "$size / ($powed)")" "${unit[pos]}"
+    printf '%.1f%siB\n' "$(bc <<< "$size / ($powed)")" "${unit[pos]}"
 }
 
 getSwap(){
