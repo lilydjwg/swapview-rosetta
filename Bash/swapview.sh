88be354c77be515b06a01930084e278f31cdaf0c
Mingye Wang <arthur200126@gmail.com>
2016-01-29 05:11:54 -0500

    Someone asked for more bash

--- Bash/swapview.sh
+++ Bash/swapview.sh
@@ -1,61 +1,44 @@
 #!/bin/bash
 
-function filesize(){
-    size=$1
-    if [[ $1 -le 1100 ]] ; then
-        echo "${size}B"
+filesize(){
+    [[ $1 -eq $1 ]] || return    # msk-num-check
+    declare -i  nunit=4 size="$1" base=1024 pos # uint64
+    declare -a  'unit=(K M G T)' # msk-hint: !mut; assert nunit == ${#unit[@]}
+    declare -ai levels
+    if ((size < 1100)); then
+        printf '%s\n' "${size}B"
         return
     fi
-    arr=($(bc -l <<EOF
-left=$size;
-unit=-1;
-while(left>1100 && unit<3){
-    left /= 1024
-    unit += 1
-}
-left
-unit
-EOF
-))
-    left=${arr[0]}
-    unit=${arr[1]}
-    units="KMGT"
-    printf "%.1f%siB\n" "$left" "${units:$unit:1}"
+    # Use k=1 .... k *= 1024 to avoid unnecessary pow-ing.
+    for ((pos=0; size / pos ** base > 1100 && pos < nunit; pos++)); do :; done
+    printf '%.2g%siB\n' "$(bc <<< "$size / ($pos ^ $base)")" "${unit[pos]}"
 }
 
-function getSwap(){
-    sumfile=/tmp/sum$RAMDOM
-    > $sumfile
+getSwap(){
+    declare -i sumsize
+    local sumfile=/tmp/sum.$$.$RAMDOM
     cd /proc
-    (for pid in [0-9]*; do
-        command=$(tr '\0' ' ' 2>/dev/null </proc/$pid/cmdline)
-        [[ $? -ne 0 ]] && continue
-        len=$((${#command}-1))
-        if [[ "${command:$len:1}"x = " "x ]]; then
-            command="${command:0:$len}"
-        fi
-
+    (declare -i sum=0; for pid in [0-9]*; do
+        # FIXME: better if printf('%q ', read(/proc/$pid/cmdline).split(NUL))
+        #        splitnul: https://stackoverflow.com/questions/8677546/
+        command=$(tr '\0' ' ' 2>/dev/null </proc/$pid/cmdline) &&
+        command=${command%' '} &&
+        
+        # assuming awk works faster than read.
         swap=$(
             awk '
                 BEGIN  { total = 0 }
                 /Swap/ { total += $2 }
                 END    { print total }
             ' /proc/$pid/smaps 2>/dev/null
-        )
-        [[ $? -ne 0 ]] && continue
+        ) &&
 
         if (( swap > 0 )); then
-            fs=$(filesize $((swap*1024)))
-            echo $swap >>$sumfile
-            printf "%5s %9s %s\n" "$pid" "$fs" "$command"
-        fi
-    done) | sort -k2 -h
-    sumsize=$(paste -sd+ <$sumfile)
-    if [[ "$sumsize"x = ""x ]]; then
-        sumsize=0
-    fi
-    total=$(filesize $(( $(echo $sumsize | bc) *1024)))
-    printf "Total: %8s\n" $total
+            sum+=swap
+            printf "%5s %9s %s\n" "$pid" "$(filesize $((swap*1024)))" "$command"
+        fi || continue
+    done; printf '%s\n' $((sum * 1024)) > $sumfile) | sort -k2 -h
+    printf "Total: %8s\n" "$(filesize $(<$sumfile) * 1024)))"
     rm $sumfile
 }
 
