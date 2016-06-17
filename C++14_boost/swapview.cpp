52ed01395f58a86422466cdaf5055c95f79743f1
Evince <baneyue@gmail.com>
2016-06-17 17:53:12 +0800

    optimize read file

--- C++14_boost/swapview.cpp
+++ C++14_boost/swapview.cpp
@@ -30,7 +30,11 @@ auto filesize(double size) {
 
 swap_info getSwapFor(int pid){
     ifstream cmd_inf("/proc/" + to_string(pid) + "/cmdline");
-    string comm((istreambuf_iterator<char>(cmd_inf)), istreambuf_iterator<char>());
+	cmd_inf.seekg(0, std::ios::end);
+	size_t size = cmd_inf.tellg();
+	string comm(size, ' ');
+	cmd_inf.seekg(0);
+	cmd_inf.read(&comm[0], size);
     if(!comm.empty()) {
         if(comm.back() == 0) comm.pop_back();
         replace(comm.begin(), comm.end(), '\0' , ' ');
