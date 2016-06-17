c8eb3fc4102bcbb8ca56ecb1a9859a3de4aed6d3
Evince <baneyue@gmail.com>
2016-06-17 17:52:41 +0800

    fix compile error and optimize read file

--- C++14/swapview.cpp
+++ C++14/swapview.cpp
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <error.h>
+#include <vector>
 
 using namespace std;
 
@@ -40,7 +41,11 @@ bool starts_with (string const& s1, string const& s2) {
 
 swap_info getSwapFor(int pid){
     ifstream cmd_inf("/proc/" + to_string(pid) + "/cmdline");
-    string comm((istreambuf_iterator<char>(cmd_inf)), istreambuf_iterator<char>());
+    cmd_inf.seekg(0, std::ios::end);
+    size_t size = cmd_inf.tellg();
+    string comm(size, ' ');
+    cmd_inf.seekg(0);
+    cmd_inf.read(&comm[0], size);
     if(!comm.empty()) {
         if(comm.back() == 0) comm.pop_back();
         replace(comm.begin(), comm.end(), '\0' , ' ');
