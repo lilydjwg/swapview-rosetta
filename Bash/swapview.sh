0757090d51562bcb3d785e3cfe4281420b2d1a16
Mingye Wang (Arthur2e5) <arthur200126@gmail.com>
2017-08-05 17:38:49 -0400

    Bash: quotes

--- Bash/swapview.sh
+++ Bash/swapview.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
-
 # globalized consts
 base=1024
 unit=(K M G T) nunit=4 # assert nunit == ${#unit[@]}
 filesize(){
-    [[ $1 -eq $1 ]] || return    # msk-num-check
-    local size="$1" pos powed # uint64, also declare -i
+    # [[ $1 -eq $1 ]] || return    # msk-num-check; trust the input now?
+    local pos powed # int64, also declare -i
+    declare -i size="$1"
     if ((size < 1100)); then
         printf '%s\n' "${size}B"
         return
     fi
     # See also ``base ** pow`` (bash) and ``base ^ pow`` (bc).
+	# Should this be precomputed?
     for ((pos=0, powed=1024; size / powed > 1100 && pos < nunit; pos++, powed *= 1024)); do :; done
     printf '%.1f%siB\n' "$(bc <<< "$size / ($powed)")" "${unit[pos]}"
 }
 
 getSwap(){
-    declare -i sumsize
-    local sumfile=/tmp/sum.$$.$RAMDOM
+    local sumfile=/tmp/sum.$$.$RANDOM
     cd /proc
+	
     (declare -i sum=0; for pid in [0-9]*; do
         # FIXME: better if printf('%q ', read(/proc/$pid/cmdline).split(NUL))
         #        splitnul: https://stackoverflow.com/questions/8677546/
         command=$(tr '\0' ' ' 2>/dev/null </proc/$pid/cmdline) &&
         command=${command%' '} &&
-        
+
         # assuming awk works faster than read.
         swap=$(
             awk '
                 BEGIN   { total = 0 }
                 /Swap:/ { total += $2 }
                 END     { print total }
-            ' /proc/$pid/smaps 2>/dev/null
+            ' "/proc/$pid/smaps" 2>/dev/null
         ) &&
 
         if (( swap > 0 )); then
             sum+=swap
-            printf "%5s %9s %s\n" "$pid" "$(filesize $((swap*1024)))" "$command"
+            printf '%5s %9s %s\n' "$pid" "$(filesize $((swap*1024)))" "$command"
         fi || continue
     done; printf '%s\n' $((sum * 1024)) > $sumfile) | sort -k2 -h
-    printf "Total: %8s\n" "$(filesize $(<$sumfile))"
-    rm $sumfile
+    printf "Total: %8s\n" "$(filesize "$(<$sumfile)")"
+    rm "$sumfile"
 }
 
 printf "%5s %9s %s\n" PID SWAP COMMAND
