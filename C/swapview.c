07cc3e2c14652379b0fae3e3acd5399f17cfcef3
Nick <znhihgiasy@gmail.com>
2018-05-25 12:19:30 +0800

    Revert "Fix cmdline."
    This reverts commit 769e6f7acd2acd9e250e284f51fbec4dc18b5361.

--- C/swapview.c
+++ C/swapview.c
@@ -15,9 +15,8 @@
 } while (false)
 
 #define PROC_PATH "/proc"
-#define SMAPS_FIELD "Swap:"
+#define COMM_LEN 17
 #define PID_LEN 6
-#define BUFSZ 128
 
 typedef char pidstr_t[PID_LEN];
 
@@ -56,30 +55,17 @@ bool gen_pids(pidstr_t* ppid) {
 typedef struct swap_info swap_info_t;
 struct swap_info {
     pidstr_t pid;
-    char* cmdline;
-    size_t swap_size;
+    char comm[COMM_LEN];
+    int swap_size;
 };
 
-char* read_cmdline(const char* fname) {
-    char* buf = (char*) malloc(BUFSZ);
-    size_t cap = BUFSZ;
-    FILE* f = fopen(fname, "r");
-    if (!f) {
-        *buf = '\0';
-        return buf;
-    }
+void read_comm(FILE* f, char comm[COMM_LEN]) {
     size_t read = 0;
     int c;
-    while (c = getc(f), c != EOF) {
-        if (c == '\0') c = ' ';
-        if (read == cap) {
-            buf = realloc(buf, cap *= 2);
-            require(buf != NULL);
-        }
-        buf[read++] = c;
+    while (c = fgetc(f), c != EOF && read < COMM_LEN - 1) {
+        comm[read++] = c;
     }
-    buf[read] = '\0';
-    return buf;
+    comm[read] = '\0';
 }
 
 bool match(FILE* f, const char* str) {
@@ -94,28 +80,33 @@ bool match(FILE* f, const char* str) {
     return true;
 }
 
-size_t read_swap_size(const char* fname) {
-    FILE* f = fopen(fname, "r");
-    if (!f) return 0;
-    size_t swap_size = 0;
-    while (match(f, SMAPS_FIELD)) {
-        size_t i;
-        require(fscanf(f, "%zu", &i) == 1);
+int read_swap_size(FILE* f) {
+    int swap_size = 0;
+    while (match(f, "Swap:")) {
+        int i;
+        require(fscanf(f, "%d", &i) == 1);
         swap_size += i * 1024;
     }
     return swap_size;
 }
 
 swap_info_t get_swap_info(const pidstr_t pid) {
-    swap_info_t retval;
+    swap_info_t retval = { "", "", 0 };
     memcpy(retval.pid, pid, PID_LEN);
     char path[sizeof(PROC_PATH) + PID_LEN + 10];
     sprintf(path, PROC_PATH "/%s/", pid);
     char* cwd = path + strlen(path);
-    strcpy(cwd, "cmdline");
-    retval.cmdline = read_cmdline(path);
+    strcpy(cwd, "comm");
+    FILE* f;
+    if ((f = fopen(path, "r"))) {
+        read_comm(f, retval.comm);
+        fclose(f);
+    }
     strcpy(cwd, "smaps");
-    retval.swap_size = read_swap_size(path);
+    if ((f = fopen(path, "r"))) {
+        retval.swap_size = read_swap_size(f);
+        fclose(f);
+    }
     return retval;
 }
 
@@ -150,9 +141,6 @@ void append(sibuffer_t** pbuf, swap_info_t si) {
 }
 
 void del_sibuffer(sibuffer_t* buf) {
-    for (size_t i = 0; i < buf->len; i++) {
-        free(buf->data[i].cmdline);
-    }
     free(buf);
 }
 
@@ -172,23 +160,23 @@ sibuffer_t* get_all_swap_infos() {
     return buf;
 }
 
-void print_file_size(double size) {
+void print_file_size(int size) {
     int unit = 0;
     char units[] = " KMGT";
     while (size > 1024 && unit < 5) {
         size /= 1024;
         unit++;
     }
-    if (!unit) printf("%lgB", size);
-    else printf("%lg%cB", size, units[unit]);
+    if (!unit) printf("%dB", size);
+    else printf("%d%cB", size, units[unit]);
 }
 
 int main() {
     printf("%5s %9s %s\n", "PID", "SWAP", "COMMAND");
-    double total = 0;
+    int total = 0;
     sibuffer_t* sis = get_all_swap_infos();
     for (size_t i = 0; i < sis->len; i++) {
-        printf("%5s %9zu %s\n", sis->data[i].pid, sis->data[i].swap_size, sis->data[i].cmdline);
+        printf("%5s %9d %s\n", sis->data[i].pid, sis->data[i].swap_size, sis->data[i].comm);
         total += sis->data[i].swap_size;
     }
     del_sibuffer(sis);
