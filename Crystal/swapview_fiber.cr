30a6e5fed1be39be29e3075965723e458aa2e070
wakeofwind <wakeofwind@outlook.com>
2019-05-26 19:02:12 +0800

    Crystal: Fix concurrent with fiber

--- Crystal/swapview_fiber.cr
+++ Crystal/swapview_fiber.cr
@@ -3,14 +3,21 @@ require "./common.cr"
 module SwapView
   def get_swap
     pids = get_pids
-    outputs = parallel(
-      pids.each {|pid| get_swap_for(pid) }
-    )
 
-    result = [] of Tuple(Int32, Int32, String | Nil)
-    outputs.each do |output|
-      if output && output[1] > 0
-        result << output
+    channel = Channel(Tuple(Int32, Int32, String | Nil)).new(pids.size)
+
+    pids.each do |pid|
+      spawn do
+        swap_info = get_swap_for(pid)
+        channel.send(swap_info)
+      end
+    end
+
+    result = [] of {Int32, Int32, String | Nil}
+    pids.size.times do
+      swap_info = channel.receive
+      if swap_info && swap_info[1] > 0
+        result << swap_info
       end
     end
     result.sort_by! {|x| x[1] }
