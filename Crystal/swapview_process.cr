4e63de604b012db6c5813e93f591495d83e365ed
wakeofwind <wakeofwind@outlook.com>
2019-05-27 23:18:11 +0800

    Crystal: Use msgpack to share non-value types among processes

--- Crystal/swapview_process.cr
+++ Crystal/swapview_process.cr
@@ -1,45 +1,18 @@
-require "json"
 require "system"
 
-require "parallel"
+require "parallel_worker"
 require "./common.cr"
 
 module SwapView
-  def worker(in_channel, out_channel)
-    loop do
-      pid = in_channel.receive
-      swap_info = get_swap_for(pid)
-      # FIXME It is hard to share non-value types at the moment.
-      # So we just drop the third element (cmd, String type).
-      # See: Serialization for non-value types #5 https://github.com/RX14/parallel.cr/issues/5 .
-      # And https://crystal-lang.org/api/0.28.0/Value.html .
-      output = {swap_info[0], swap_info[1]}
-      out_channel.send(output)
-    end
-  end
-
   def get_swap
     pids = get_pids
 
-    job_channel = PChan(Int32).new
-    results_channel = PChan(Tuple(Int32, Int32)).new
-
-    System.cpu_count.times do
-      pspawn { worker(job_channel, results_channel) }
+    worker = ParallelWorker::Process(Int32, Tuple(Int32, Int32, String | Nil)).new(System.cpu_count.to_i32) do |pid|
+      get_swap_for(pid)
     end
 
-    spawn do
-      pids.each { |pid| job_channel.send pid }
-    end
-
-    result = [] of {Int32, Int32, Nil}
-    pids.size.times do
-      swap_info = results_channel.receive
-      next if !swap_info
-      if swap_info[1] > 0
-        result << {swap_info[0], swap_info[1], nil}
-      end
-    end
+    result = worker.perform_all(pids)
+    result.select!  { |swap_info| swap_info && swap_info[1] > 0 }
 
     result.sort_by! { |x| x[1] }
     result
