595afa0035a00a08b0224e5c52c5c9353b4f5ecc
hellojukay <licong@qianxin.com>
2021-11-25 09:13:35 +0800

    1. perltidy 格式化代码 2. 创建和 cpu 数量等同的线程

--- Perl/swapview.pl
+++ Perl/swapview.pl
@@ -5,24 +5,18 @@ use warnings;
 use utf8;
 use autodie;
 use 5.010;
-use threads;
-use threads::shared;
-use Thread::Queue;
+
 use English qw{ -no_match_vars }; # Avoids regex performance penalty in perl 5.16 and earlier.
 
-my $results  = Thread::Queue->new();
-my $count :shared =0;
-
-sub get_process_swap ($) {
-    my $q = shift;
-    while(1) {
-        my $pid = $q->dequeue();
-        last unless($pid);
+sub get_swap_info_from {
+    my ( $pid_dirs_ref ) = @_;
+    my @results = ();
+    for my $pid ( @{$pid_dirs_ref} ) {
         my $smaps_file = join q{/}, $pid, q{smaps};
         my $swap_size = 0;
         if ( -f $smaps_file and
-            -r $smaps_file and
-            ( $EUID == 0 or -O $smaps_file )
+             -r $smaps_file and
+             ( $EUID == 0 or -O $smaps_file )
         ) {
             open my $smaps_file_fh, '<', $smaps_file;
             while (my $line = <$smaps_file_fh>) {
@@ -36,8 +30,8 @@ sub get_process_swap ($) {
         my $cmdline_file =  join q{/}, $pid, q{cmdline};
         my $cmdline;
         if ( -f $cmdline_file and
-            -r $cmdline_file and
-            ( $EUID == 0 or -O $cmdline_file )
+             -r $cmdline_file and
+             ( $EUID == 0 or -O $cmdline_file )
         ) {
             open my $cmdline_file_fh, '<', $cmdline_file;
             $cmdline = do {
@@ -48,27 +42,16 @@ sub get_process_swap ($) {
             $cmdline =~ s/\0$//;
             $cmdline =~ s/\0/ /g;
         }
-        {
-            lock($count);
-            $count = $count+1;
-        }
-        $results->enqueue({ pid  => $pid, swap => $swap_size, cmd  => $cmdline, });
+        push @results, { pid  => $pid,
+                         swap => $swap_size,
+                         cmd  => $cmdline,
+                       };
     }
-
-}
-sub get_swap_info_from ($) {
-    my $q = Thread::Queue->new();
-    foreach (1..8){
-        threads->create(\&get_process_swap,$q)
-    }
-    $q->enqueue($_) foreach(@{$_[0]});
-    $q->end();
-    $_->join() foreach(threads->list());
-    return $results->extract(0,$count);
+    return @results;
 }
 
-sub convert_file_size_from_kB ($) {
-    my  $size  = shift;
+sub convert_file_size_from_kB {
+    my ( $size ) = @_;
     my @units = qw{K M G T};
     my $unit = 0;
     while ( $size > 1100 and $unit < 3 ) {
@@ -80,11 +63,14 @@ sub convert_file_size_from_kB ($) {
 
 sub main {
     my $printf_format = qq{%7s %9s %s\n};
-    my @pid_dirs = glob "/proc/[0123456789]*";
-    my @sorted_results = sort { $a->{'swap'} <=> $b->{'swap'} } get_swap_info_from( \@pid_dirs );
+    opendir( my $proc_dh, q{/proc} );
+    chdir $proc_dh;
+    my @pid_dirs = grep { -d && /^[[:digit:]]+$/xms } readdir $proc_dh;
+    closedir $proc_dh;
+    my @results = sort { $a->{'swap'} <=> $b->{'swap'} } get_swap_info_from( \@pid_dirs );
     my $total_swap_size = 0;
     printf $printf_format, qw{PID SWAP COMMAND};
-    for my $result ( @sorted_results ) {
+    for my $result ( @results ) {
         $total_swap_size += $result->{'swap'};
         printf $printf_format, $result->{'pid'}, convert_file_size_from_kB( $result->{'swap'} ), $result->{'cmd'};
     }
