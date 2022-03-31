8fbdd706c8c28a33a51b2cddd60cdc3adbb3e21a
ZhangHao <official@antigen.top>
2022-03-31 16:53:40 +0800

    Racket_parallel:improve the performance (#177)
    Racket*: multiple updates

--- Racket_parallel/swapview.rkt
+++ Racket_parallel/swapview.rkt
@@ -1,16 +1,63 @@
-
 #lang racket/base
-(require (only-in racket/format ~a ~r))
-(require (only-in racket/place place place-channel-get place-channel-put))
-(require (only-in racket/list empty empty?))
-(require (only-in racket/math exact-floor))
-(require (only-in racket/string string-split string-replace string-prefix?))
-(require (only-in racket/file file->lines file->string))
+(require
+  (only-in racket/file file->string)
+  (only-in racket/format ~a ~r)
+  (only-in racket/list split-at)
+  (only-in racket/math exact-floor)
+  (only-in racket/string string-replace))
 (provide main)
-;;use "racket -tm compiled/swapview_rkt.zo"
 
-(define pid-list (filter string->number
-                         (map path->string (directory-list "/proc"))))
+(module p racket/base
+  (require (only-in racket/file file->lines)
+           (only-in racket/place place place-channel-put place-channel-get)
+           (only-in racket/string string-split string-prefix?))
+  (provide getSmaps getSize parallel place-channel-put place-channel-get)
+  (define getSmaps (lambda (pid)
+                     (file->lines (format "/proc/~a/smaps" pid))))
+  (define getSize (lambda (smaps)
+                    (* 1024 (apply + (map (lambda (s) (cond [(string-prefix? s "Swap:") (string->number (cadr (string-split s)))]
+                                                            [else 0])) smaps)))))
+  (define (parallel)
+    (place ch
+           (define latter (place-channel-get ch))
+           (place-channel-put ch (map (lambda (pid) (with-handlers ([exn:fail:filesystem? (lambda (exn) 0)])
+                                                      (getSize (getSmaps pid)))) latter)))
+    ))
+
+(require 'p)
+
+(define (fmtPid pid) (~a pid  #:width 7 #:align 'right))
+(define (filesize n)
+  (if (< n 1100) (format "~aB" n)
+      (letrec ([p (exact-floor (log (/ n 1100) 1024))]
+               [s (~r (/ n (expt 1024 (add1 p))) #:precision '(= 1))]
+               [unit (string-ref "KMGT" p)])
+        (format "~a~aiB" s unit))))
+(define (fmtSize size) (~a size  #:width 9 #:align 'right))
+(define (strinit s)
+  (let ([s (string-replace s "\x0" " ")]
+        [l (string-length s)])
+    (if (zero? l) s (substring s 0 (- l 1)))))
+
+(define pl (parallel))
+(define pid-list (filter string->number (map path->string (directory-list "/proc"))))
+(define len (exact-floor (* (length pid-list) 2/3)))
+(define-values (former latter) (split-at pid-list len))
+(define-values (size-list result-list)
+  (begin
+    (place-channel-put pl latter)
+    (let ((former (map (lambda (pid) (with-handlers ([exn:fail:filesystem? (lambda (exn) 0)])
+                                       (getSize (getSmaps pid)))) former))
+          (cmdline-list (map (lambda (pid) (with-handlers ([exn:fail:filesystem? (lambda (exn) "")])
+                                             (file->string (format "/proc/~a/cmdline" pid))))
+                             pid-list)))
+      (define size-list (append former (place-channel-get pl)))
+      (values size-list (map (lambda (pid size cmd) (list pid size cmd)) pid-list size-list cmdline-list)))))
+
+(define format-result (map (lambda (result) (list ((compose fmtPid car) result)
+                                                  ((compose fmtSize filesize cadr) result)
+                                                  ((compose strinit caddr) result)))
+                           (sort (filter (lambda (result) (not (zero? (cadr result)))) result-list) #:key cadr <)))
 
 (define (fmt1 s1 s2 s3)
   (begin
@@ -18,69 +65,13 @@
                        s2 " "
                        s3))
     (newline)))
-
-(define (fmtPid pid)
-  (~a pid  #:width 7 #:align 'right))
-
-(define (filesize n)
-  (if (< n 1100) (format "~aB" n)
-      (letrec ([p (exact-floor (log (/ n 1100) 1024))]
-               [s (~r (/ n (expt 1024 (add1 p))) #:precision '(= 1))]
-               [unit (string-ref "KMGT" p)])
-        (format "~a~aiB" s unit))))
-
-(define (fmtSize size)
-  (~a size #:width 9 #:align 'right))
-
 (define (total n)
   (begin
     (display "Total: ")
     (display (~a n  #:min-width 10 #:align 'right))
     (newline)))
 
-(define (strinit s)
-  (let ([l (string-length s)])
-    (if (zero? l) s (substring s 0 (- l 1)))))
-
-(define (getSwapFor)
-  (define pl
-    (place ch
-           (place-channel-put ch
-                              (map
-                               (lambda (pid) (with-handlers ([exn:fail:filesystem? (lambda (e) "")])
-                                               (strinit (string-replace
-                                                         (file->string
-                                                          (format "/proc/~a/cmdline" pid)) "\x0" " "))
-                                               ))
-                               pid-list))))
-  (let ((format-pid (map fmtPid pid-list))
-        (size-list
-         (letrec ([swap? (lambda (l) (string-prefix? l "Swap:"))]
-                  [getSize (lambda (l) (cadr (string-split l)))]
-                  [getSmaps (lambda (pid)
-                              (with-handlers ([exn:fail:filesystem? (lambda (e) empty)])
-                                (filter swap? (file->lines (format "/proc/~a/smaps" pid)))))]
-                  [loop (lambda (pid-list)
-                          (cond
-                            [(empty? pid-list) empty]
-                            [else (cons (* 1024 (apply + (map (compose string->number getSize) (getSmaps (car pid-list)))))
-                                        (loop (cdr pid-list)))]))])
-           (loop pid-list)))
-        (cmd-list (place-channel-get pl)))
-    (map (lambda (pid size cmd) (list pid size cmd)) format-pid size-list cmd-list)))
-
-(define (getSwap)
-  (begin
-    (sort (filter (lambda (l) (> (cadr l) 0))
-                  (getSwapFor))
-          #:key cadr <)))
-
 (define (main)
-  (define results (getSwap))
-  (let ((pid-list (map car results))
-        (size-list (map cadr results))
-        (cmd-list (map caddr results)))
-    (begin
-      (fmt1 (fmtPid "PID") (fmtSize "SWAP") "COMMAND")
-      (map (lambda (pid size cmd) (fmt1 pid ((compose fmtSize filesize) size) cmd)) pid-list size-list cmd-list)
-      (total (filesize (apply + size-list))))))
+  (fmt1 (fmtPid "PID") (fmtSize "SWAP") "COMMAND")
+  (map (lambda (result) (apply fmt1 result)) format-result)
+  (total (filesize (apply + size-list))))
